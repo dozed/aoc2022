b40c918e5c6177cdc178f8a9f1fc64a2c793cbbc
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-11 22:28:34 +0100

    Added another example input

--- src/Day9.hs
+++ src/Day9.hs
@@ -9,8 +9,8 @@ import Text.RawString.QQ
 
 import Util (regularParse)
 
-testInput :: String
-testInput = [r|R 4
+testInput1 :: String
+testInput1 = [r|R 4
 U 4
 L 3
 D 1
@@ -18,6 +18,17 @@ R 4
 D 1
 L 5
 R 2
+|]
+
+testInput2 :: String
+testInput2 = [r|R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
 |]
 
 data MoveSpec = MoveUpSpec Int

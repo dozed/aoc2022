7178944b73a83ef8c8184d796e4820108429b164
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-12 17:26:55 +0100

    Day 9 part 2

--- src/Day9.hs
+++ src/Day9.hs
@@ -2,7 +2,7 @@
 
 module Day9 where
 
-import Data.List (nub)
+import Data.List (nub, transpose)
 import Text.Parsec
 import Text.Parsec.String
 import Text.RawString.QQ
@@ -61,6 +61,7 @@ isAdjacent (x1, y1) (x2, y2) = abs (x1 - x2) <= 1 && abs (y1 - y2) <= 1
 
 data Move = MoveUp | MoveDown | MoveLeft | MoveRight
           | MoveUpRight | MoveDownRight | MoveDownLeft | MoveUpLeft
+          | Stay
           deriving (Eq, Show)
 
 genMoves :: [MoveSpec] -> [Move]
@@ -71,6 +72,7 @@ genMoves ((MoveLeftSpec n) : others) = replicate n MoveLeft <> genMoves others
 genMoves ((MoveRightSpec n) : others) = replicate n MoveRight <> genMoves others
 
 applyMove :: Pos -> Move -> Pos
+applyMove (x, y) Stay = (x, y)
 applyMove (x, y) MoveUp = (x, y+1)
 applyMove (x, y) MoveDown = (x, y-1)
 applyMove (x, y) MoveLeft = (x-1, y)
@@ -80,26 +82,35 @@ applyMove (x, y) MoveDownRight = (x+1, y-1)
 applyMove (x, y) MoveDownLeft = (x-1, y-1)
 applyMove (x, y) MoveUpLeft = (x-1, y+1)
 
-getMoveForTail :: Pos -> Pos -> Maybe Move
+getMoveForTail :: Pos -> Pos -> Move
 getMoveForTail headPos@(hx, hy) tailPos@(tx, ty)
-  | isAdjacent headPos tailPos = Nothing
-  | hy == ty = if hx < tx then Just MoveLeft else Just MoveRight
-  | hx == tx = if hy < ty then Just MoveDown else Just MoveUp
-  | hx > tx && hy > ty = Just MoveUpRight
-  | hx < tx && hy > ty = Just MoveUpLeft
-  | hx < tx && hy < ty = Just MoveDownLeft
-  | hx > tx && hy < ty = Just MoveDownRight
+  | isAdjacent headPos tailPos = Stay
+  | hy == ty = if hx < tx then MoveLeft else MoveRight
+  | hx == tx = if hy < ty then MoveDown else MoveUp
+  | hx > tx && hy > ty = MoveUpRight
+  | hx < tx && hy > ty = MoveUpLeft
+  | hx < tx && hy < ty = MoveDownLeft
+  | hx > tx && hy < ty = MoveDownRight
 
 applyMoveToHeadAndTail :: Pos -> Pos -> Move -> (Pos, Pos)
-applyMoveToHeadAndTail headPos tailPos moveHead =
-  let headPos' = applyMove headPos moveHead
+applyMoveToHeadAndTail headPos tailPos headMove =
+  let headPos' = applyMove headPos headMove
       moveTail = getMoveForTail headPos' tailPos
-      tailPos' = maybe tailPos (applyMove tailPos) moveTail
+      tailPos' = applyMove tailPos moveTail
   in (headPos', tailPos')
+
+updateKnotPositions :: Move -> [Pos] -> [Pos]
+updateKnotPositions headMove [] = []
+updateKnotPositions headMove (headPos : tailPos : otherPos) =
+  let headPos' = applyMove headPos headMove
+      moveTail = getMoveForTail headPos' tailPos
+  in headPos' : updateKnotPositions moveTail (tailPos : otherPos)
+updateKnotPositions headMove (headPos : []) = [applyMove headPos headMove]
 
 day9 :: IO ()
 day9 = do
-  -- let input = testInput
+  -- let input = testInput1
+  -- let input = testInput2
   input <- readFile "input/Day9.txt"
 
   moveSpecs <- case regularParse parseMoveSpecs input of
@@ -119,5 +130,22 @@ day9 = do
   let (headPositions, tailPositions) = unzip positions
   print headPositions
   print tailPositions
+  print $ nub tailPositions
+  print $ length . nub $ tailPositions
+
+  -- part 2
+  let positions = map (\_ -> mkPos 0 0) [0..9]
+  let headMove = head moves
+
+  -- for each move:
+  -- - apply move to head
+  -- - for each pair p in [(0, 1), (1, 2), ..., (8, 9)]
+  --   - generate move based on new position of predecessor knot and old position of current knot
+  --   - apply moves to pairs
+  -- let updatedPositions = updateKnotPositions headMove positions
+  let updatedPositions = scanl (\pos headMove -> updateKnotPositions headMove pos) positions moves
+  let tailPositions = (!! 9) . transpose $ updatedPositions
+  
+  print $ tailPositions
   print $ nub tailPositions
   print $ length . nub $ tailPositions

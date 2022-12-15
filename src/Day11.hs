0989b30c41bfb7e864f14b9dc0e0eaa7b98b589a
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-15 20:27:16 +0100

    Code cleanup

--- src/Day11.hs
+++ src/Day11.hs
@@ -9,6 +9,36 @@ import Text.RawString.QQ
 
 import Util (regularParse)
 
+testInput1 :: String
+testInput1 = [r|Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+|]
+
 data Operation = MulWith Int
                | MulWithOld
                | AddWith Int
@@ -51,36 +81,6 @@ monkeyParser = do
 
 monkeysParser :: Parser [Monkey]
 monkeysParser = sepBy1 monkeyParser (char '\n')
-
-testInput1 :: String
-testInput1 = [r|Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
-
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1
-|]
 
 day11 :: IO ()
 day11 = do

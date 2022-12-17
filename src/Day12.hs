4cf5039cbb45a9c35a2ef5782ae12cffdf5e3fe7
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-17 17:20:15 +0100

    Implemented getHeight, incrCell, getCell, getAdjacentPositions, getReachablePositions for day 12

--- src/Day12.hs
+++ src/Day12.hs
@@ -2,9 +2,14 @@
 
 module Day12 where
 
-import Control.Monad (forM_)
+import Control.Applicative ((<|>))
+import Control.Monad (forM_, mfilter)
+import Data.Char (chr, ord)
 import Data.List (elemIndex)
-import Data.Maybe (fromMaybe)
+import Data.List.Extra ((!?))
+import Data.Maybe (fromMaybe, isJust)
+import Data.Set (Set)
+import qualified Data.Set as S
 import Text.RawString.QQ
 
 import Util (strip)
@@ -24,22 +29,51 @@ type Y = Int
 type X = Int
 type Pos = (Y, X)
 
+getHeight :: Cell -> Cell
+getHeight 'S' = 'a'
+getHeight 'E' = 'z'
+getHeight c = c
+
+incrCell :: Cell -> Cell
+incrCell c = chr (ord c + 1)
+
 mkField :: String -> Field
 mkField = lines
 
-getPos :: Cell -> Field -> Pos
+getPos :: Cell -> Field -> Maybe Pos
 getPos cell field = getPos' 0 field
   where
-    getPos' rowIdx (row:others) = 
-      let idx = (\colIdx -> (rowIdx,colIdx)) <$> elemIndex cell row
-      in fromMaybe (getPos' (rowIdx+1) others) idx
-    getPos' _ [] = undefined
+    getPos' :: Y -> Field -> Maybe Pos
+    getPos' rowIdx (row:others) =
+      let idx = (\colIdx -> (rowIdx, colIdx)) <$> elemIndex cell row
+      in idx <|> getPos' (rowIdx+1) others
+    getPos' _ [] = Nothing
 
-getStartPos :: Field -> Pos
+getStartPos :: Field -> Maybe Pos
 getStartPos = getPos 'S'
 
-getEndPos :: Field -> Pos
+getEndPos :: Field -> Maybe Pos
 getEndPos = getPos 'E'
+
+getCell :: Field -> Pos -> Maybe Cell
+getCell field (y, x) = do
+  row <- field !? y
+  cell <- row !? x
+  return cell
+
+getAdjacentPositions :: Pos -> Set Pos
+getAdjacentPositions (y, x) = S.fromList [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
+
+getReachablePositions :: Field -> Pos -> Set Pos
+getReachablePositions field pos =
+  case getCell field pos of
+    Nothing -> S.empty
+    Just currentCellValue ->
+      let
+        testCellValue = incrCell $ getHeight currentCellValue
+        adjacentPositions = getAdjacentPositions pos
+        reachablePositions = S.filter (isJust . mfilter (<= testCellValue) . fmap getHeight . getCell field) adjacentPositions
+      in reachablePositions
 
 day12 :: IO ()
 day12 = do
@@ -47,5 +81,19 @@ day12 = do
 
   let field = mkField input
 
+  startPos <- case getStartPos field of
+    (Just pos) -> pure pos
+    Nothing -> fail "could not get start position"
+
+  endPos <- case getEndPos field of
+    (Just pos) -> pure pos
+    Nothing -> fail "could not get end position"
+
+  -- debug
+  putStrLn "Field:"
   forM_ field $ \l -> do
     putStrLn l
+
+  putStrLn $ "startPos: " <> show startPos
+  putStrLn $ "endPos: " <> show endPos
+
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -1,17 +1,52 @@
 module Day12Spec (day12Spec) where
 
+import qualified Data.Set as S
+
 import Test.Hspec
 
 import Day12
 
 day12Spec :: Spec
 day12Spec = do
-  describe "getStartPos" $ do
-    let field = mkField testInput1
+  let field = mkField testInput1
 
+  describe "getHeight" $ do
+    it "should compute height for 'S'" $ getHeight 'S' `shouldBe` 'a'
+    it "should compute height for 'E'" $ getHeight 'E' `shouldBe` 'z'
+    it "should compute height for 'c'" $ getHeight 'c' `shouldBe` 'c'
+
+  describe "incrCell" $ do
+    it "should get next higher cell value" $ do
+      incrCell 'a' `shouldBe` 'b'
+
+  describe "getPos" $ do
+    it "should return value for cell value which is on the field" $ do
+      getPos 'a' field `shouldBe` Just (0, 1)
+
+    it "should return Nothing for cell value which is not on the field" $ do
+      getPos 'X' field `shouldBe` Nothing
+
+  describe "getStartPos" $ do
     it "should get start position" $ do
-      getStartPos field `shouldBe` (0, 0)
+      getStartPos field `shouldBe` Just (0, 0)
 
     it "should get end position" $ do
-      getEndPos field `shouldBe` (2, 5)
+      getEndPos field `shouldBe` Just (2, 5)
 
+  describe "getCell" $ do
+    it "should get cell" $ do
+      getCell field (0, 0) `shouldBe` Just 'S'
+      getCell field (2, 5) `shouldBe` Just 'E'
+      getCell field (1, 0) `shouldBe` Just 'a'
+      getCell field (0, 1) `shouldBe` Just 'a'
+
+    it "should return Nothing for out-of-field cell" $ do
+      getCell field (12, 15) `shouldBe` Nothing
+
+  describe "getAdjacentPositions" $ do
+    it "should compute adjacent positions" $ do
+      getAdjacentPositions (0, 0) `shouldBe` S.fromList [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+  describe "getReachablePositions" $ do
+    it "should compute reachable positions" $ do
+      getReachablePositions field (0, 0) `shouldBe` S.fromList [(0, 1), (1, 0)]

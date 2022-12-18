13372f27def91e43d7cbb9fcd190edcfc90e6845
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-18 21:08:38 +0100

    Implemented Packet parsers

--- src/Day13.hs
+++ src/Day13.hs
@@ -2,6 +2,7 @@
 
 module Day13 where
 
+import Control.Monad (void)
 import Text.Parsec
 import Text.Parsec.String
 import Text.RawString.QQ
@@ -34,16 +35,34 @@ testInput1 = lstrip [r|
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]
 |]
-  
-  
+
 data Packet = L [Packet]
             | S Int
             deriving (Eq, Show)
 
--- [1,1,3,1,1]
+smallPacketParser :: Parser Packet
+smallPacketParser = S . read <$> many1 digit
+
+largePacketParser :: Parser Packet
+largePacketParser = do
+  void $ char '['
+  packets <- sepBy1 packetParser (char ',')
+  void $ char ']'
+  return $ L packets
 
 packetParser :: Parser Packet
-packetParser = undefined
+packetParser = largePacketParser <|> smallPacketParser
+
+packetPairParser :: Parser (Packet, Packet)
+packetPairParser = do
+  p1 <- largePacketParser
+  void endOfLine
+  p2 <- largePacketParser
+  void endOfLine
+  return (p1, p2)
+
+packetPairsParser :: Parser [(Packet, Packet)]
+packetPairsParser = sepBy1 packetPairParser endOfLine 
 
 day13 :: IO ()
 day13 = do
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -1,14 +1,53 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day13Spec (day13Spec) where
+
+import Text.RawString.QQ
 
 import Test.Hspec
 
 import Day13
-import Util (regularParse)
+import Util (lstrip, regularParse)
+
+txt1 :: String
+txt1 = lstrip [r|
+[1,1,3,1,1]
+[[1],[2,3,4]]
+|]
+
+txt2 :: String
+txt2 = lstrip [r|
+[1,1,3,1,1]
+[[1],[2,3,4]]
+
+[9]
+[[8,7,6]]
+|]
+
+packet1 :: Packet
+packet1 = L [S 1, S 1, S 3, S 1, S 1]
+
+packet2 :: Packet
+packet2 = L [L [S 1], L [S 2, S 3, S 4]]
+
+packet3 :: Packet
+packet3 = L [S 9]
+
+packet4 :: Packet
+packet4 = L [L [S 8, S 7, S 6]]
 
 day13Spec :: Spec
 day13Spec = do
 
   describe "packetParser" $ do
     it "should parse a packet" $ do
-      regularParse packetParser "[1,1,3,1,1]" `shouldBe` Right (L [S 1, S 1, S 3, S 1, S 1])
-      regularParse packetParser "[[1],[2,3,4]]" `shouldBe` Right (L [L [S 1], L [S 2, S 3, S 4]])
+      regularParse packetParser "[1,1,3,1,1]" `shouldBe` Right packet1
+      regularParse packetParser "[[1],[2,3,4]]" `shouldBe` Right packet2
+
+  describe "packetPairParser" $ do
+    it "should parse a packet pair" $ do
+      regularParse packetPairParser txt1 `shouldBe` Right (packet1, packet2)
+
+  describe "packetPairsParser" $ do
+    it "should parse packet pairs" $ do
+      regularParse packetPairsParser txt2 `shouldBe` Right [(packet1, packet2), (packet3, packet4)]

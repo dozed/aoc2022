c592ec1b0dc68f45b32cbec05a2047a3f116f8a9
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-30 23:32:28 +0100

    Code cleanup

--- src/UtilGraphSearch.hs
+++ src/UtilGraphSearch.hs
@@ -9,22 +9,6 @@ import qualified Data.Set as S
 
 type Predecessors a = Map a a
 
-bfs' :: Ord a => (a -> Set a) -> a -> [a] -> Set a -> Predecessors a -> Predecessors a
-bfs' getNeighbours current toVisit visited predecessors =
-  let visited' = S.insert current visited
-      reachableNodes = getNeighbours current
-      reachableNodesNotVisited = S.difference reachableNodes visited'
-      reachableNodesNotVisitedAndNotToVisit = S.difference reachableNodesNotVisited (S.fromList toVisit)
-      toVisit' = toVisit ++ S.toList reachableNodesNotVisitedAndNotToVisit
-      predecessors' = foldl (\acc x -> M.insert x current acc) predecessors reachableNodesNotVisitedAndNotToVisit
-
-  in case toVisit' of
-    [] -> predecessors'
-    x:xs -> bfs' getNeighbours x xs visited' predecessors'
-
-bfs :: Ord a => (a -> Set a) -> a -> Predecessors a
-bfs getNeighbours current = bfs' getNeighbours current [] S.empty M.empty
-
 getPath'' :: Ord a => Predecessors a -> a -> [a] -> [a]
 getPath'' predecessors a path =
   case M.lookup a predecessors of
@@ -44,3 +28,19 @@ getShortestPathLengths valveLabels predecessorsMap =
   let xxs = [[length (getPath predecessorsMap x y) - 1 | x <- valveLabels] | y <- valveLabels]
       matrix = MT.fromLists xxs
   in matrix
+
+bfs' :: Ord a => (a -> Set a) -> a -> [a] -> Set a -> Predecessors a -> Predecessors a
+bfs' getNeighbours current toVisit visited predecessors =
+  let visited' = S.insert current visited
+      reachableNodes = getNeighbours current
+      reachableNodesNotVisited = S.difference reachableNodes visited'
+      reachableNodesNotVisitedAndNotToVisit = S.difference reachableNodesNotVisited (S.fromList toVisit)
+      toVisit' = toVisit ++ S.toList reachableNodesNotVisitedAndNotToVisit
+      predecessors' = foldl (\acc x -> M.insert x current acc) predecessors reachableNodesNotVisitedAndNotToVisit
+
+  in case toVisit' of
+    [] -> predecessors'
+    x:xs -> bfs' getNeighbours x xs visited' predecessors'
+
+bfs :: Ord a => (a -> Set a) -> a -> Predecessors a
+bfs getNeighbours current = bfs' getNeighbours current [] S.empty M.empty

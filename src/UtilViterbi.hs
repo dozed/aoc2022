052fe4c1434e9d6bf88b3a82841d45267f66450b
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-30 20:44:18 +0100

    Added Viterbi matrix parameter to `getB`

--- src/UtilViterbi.hs
+++ src/UtilViterbi.hs
@@ -33,6 +33,7 @@ data ViterbiInfo a b = ViterbiInfo {
   -- | Get a `b` given from/to `a`s
   getB
     :: ViterbiInfo a b  -- ^ The info about the structure Viterbi is applied to
+    -> Matrix (a, b)  -- ^ The current matrix of tagged, previous values
     -> Timestep  -- ^ The current timestep
     -> a  -- ^ The `from` value for which the `b` should be computed
     -> a  -- ^ The `to` value for which the `b` should be computed
@@ -41,61 +42,35 @@ data ViterbiInfo a b = ViterbiInfo {
 }
 
 viterbiStepForValue :: (Ord a, Ord b) => ViterbiInfo a b -> Matrix (a, b) -> Int -> a -> Matrix (a, b)
-viterbiStepForValue info@ViterbiInfo { indexes, mkStart, getB } previousValves timestep@1 toValve =
-  let valveIdx = indexes M.! toValve
-      (maxPrev, b) = (mkStart, getB info timestep mkStart toValve)
-      previousValves' = MT.setElem (maxPrev, b) (valveIdx, timestep) previousValves
-  in previousValves'
-viterbiStepForValue info@ViterbiInfo { indexes, getCandidates, getB } previousValves timestep toValve =
-  let valveIdx = indexes M.! toValve
-      candidates = getCandidates info previousValves timestep toValve
-  in if null candidates then previousValves  -- some valves dont have previous values -> paths ends
+viterbiStepForValue info@ViterbiInfo { indexes, mkStart, getB } previous timestep@1 to =
+  let toIdx = indexes M.! to
+      (maxPrev, b) = (mkStart, getB info previous timestep mkStart to)
+      previous' = MT.setElem (maxPrev, b) (toIdx, timestep) previous
+  in previous'
+viterbiStepForValue info@ViterbiInfo { indexes, getCandidates, getB } previous timestep to =
+  let toIdx = indexes M.! to
+      candidates = getCandidates info previous timestep to
+  in if null candidates then previous  -- some nodes dont have previous values -> paths ends
      else
-       let pprsRemainings = map (\fromValve -> getB info timestep fromValve toValve) candidates
+       let pprsRemainings = map (\from -> getB info previous timestep from to) candidates
            xs = candidates `zip` pprsRemainings
            (maxPrev, b) = maximumBy (compare `on` snd) xs
-           -- map of previous valve at a given timestep for a given valve with a tag, e.g. remaining minutes and released pressure
-           previousValves' = MT.setElem (maxPrev, b) (valveIdx, timestep) previousValves
-       in previousValves'
+           -- map of previous nodes at a given timestep for a given node with a tag, e.g. remaining minutes and released pressure
+           previous' = MT.setElem (maxPrev, b) (toIdx, timestep) previous
+       in previous'
 
 viterbiRound :: (Ord a, Ord b) => ViterbiInfo a b -> Matrix (a, b) -> Int -> Matrix (a, b)
-viterbiRound info@ViterbiInfo { mkStart, values } previousValves timestep =
-  let valveLabels' = filter (/= mkStart) values
-  in foldl (\pv v -> viterbiStepForValue info pv timestep v) previousValves valveLabels'
+viterbiRound info@ViterbiInfo { mkStart, values } previous timestep =
+  let values' = filter (/= mkStart) values
+  in foldl (\pv v -> viterbiStepForValue info pv timestep v) previous values'
 
 viterbi :: (Ord a, Show a, Ord b, Show b) => ViterbiInfo a b -> Matrix (a, b) -> Int -> IO (Matrix (a, b))
-viterbi info@ViterbiInfo { indexes, mkEmpty, isFinished } previousValves timestep = do
+viterbi info@ViterbiInfo { indexes, mkEmpty, isFinished } previous timestep = do
   putStrLn $ "timestep: " <> show timestep
   print indexes
-  let previousValves' = appendColumn mkEmpty previousValves
-      previousValves'' = viterbiRound info previousValves' timestep
-  print previousValves''
+  let previous' = appendColumn mkEmpty previous
+      previous'' = viterbiRound info previous' timestep
+  print previous''
   putStrLn "-----"
-  if isFinished info previousValves'' timestep then return previousValves''
-  else viterbi info previousValves'' (timestep + 1)
-
--- distances :: Matrix Int,
---
--- getAccPPRAndRemaining fromValve toValve =
---   let fromIdx = indexes M.! fromValve
---       fromRemaining = remainingMinutes MT.! (fromIdx, timestep - 1)
---       (curPpr, currentRemaining) = getPotentialPressureRelease info (fromValve, fromRemaining) toValve
---       prevPpr = pprs MT.! (fromIdx, timestep - 1)
---   in (prevPpr + curPpr, currentRemaining)
---
--- getPathTo :: Matrix Label -> Map Label Int -> Label -> Int -> [Label]
--- getPathTo _ _ x 0 = [x]
--- getPathTo previousValves valveIdxs valve i =
---   let valveIdx = valveIdxs M.! valve
---       x = previousValves MT.! (valveIdx, i)
---   in valve : getPathTo previousValves valveIdxs x (i-1)
---
--- getCandidates :: [Label] -> Map Label Int -> Matrix Label -> Int -> Label -> [Label]
--- getCandidates _ _ _ 0 _ = ["AA"]
--- getCandidates valveLabels valveIdxs previousValves pathLength valve =
---   let candidates = filter (\v -> v /= valve && v /= "AA" && previousValves MT.! (valveIdxs M.! v, pathLength - 1) /= "--") valveLabels
---       -- the same valve cant be the previous valve
---       paths = map (\v -> getPathTo previousValves valveIdxs v (pathLength - 1)) candidates
---       -- a previous valve which contains valve on its path cant be the previous valve
---       candidates' = map fst . filter (\(c, p) -> valve `notElem` p) $ (candidates `zip` paths)
---   in candidates'
+  if isFinished info previous'' timestep then return previous''
+  else viterbi info previous'' (timestep + 1)

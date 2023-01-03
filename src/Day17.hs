4820415f1c8ec220f37216b4fa5d24ec8a55aaa1
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-01-03 19:51:39 +0100

    Day 17 part 1

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,11 +1,14 @@
 module Day17 (Jet(..), day17, jetsParser) where
 
 import Data.Function (on)
-import Data.List (maximumBy)
+import Data.List (intercalate, maximumBy)
 import Data.Set (Set)
 import qualified Data.Set as S
+import Debug.Trace (trace)
 import Text.Parsec
 import Text.Parsec.String
+
+import Util (regularParse)
 
 testInput :: String
 testInput = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
@@ -28,8 +31,23 @@ type Y = Int
 type Pos = (X, Y)
 type Field = Set Pos
 
+mkField :: Field
+mkField = S.fromList [(x,0) | x <- [0..8]]
+
+mkBlocks :: [Block]
+mkBlocks = cycle [HLine, Plus, L, VLine, Square]
+
+mkJets :: [Jet] -> [Jet]
+mkJets = cycle
+
 getDownPos :: Pos -> Pos
 getDownPos (x, y) = (x, y - 1)
+
+getLeftPos :: Pos -> Pos
+getLeftPos (x, y) = (x - 1, y)
+
+getRightPos :: Pos -> Pos
+getRightPos (x, y) = (x + 1, y)
 
 materialize :: Block -> Pos -> Set Pos
 materialize HLine (x, y) = S.fromList [(x, y), (x+1, y), (x+2, y), (x+3, y)]
@@ -41,11 +59,24 @@ materialize Square (x, y) = S.fromList [(x, y), (x+1, y), (x, y+1), (x+1, y+1)]
 getMaxY :: Field -> Y
 getMaxY = snd . maximumBy (compare `on` snd)
 
+drawField :: Field -> String
+drawField field =
+  let maxY = getMaxY field
+      getPixel x y
+        | y == 0 = if x == 0 || x == 8 then '+' else '-'
+        | x == 0 || x == 8 = '|'
+        | S.member (x, y) field = '#'
+        | otherwise = '.'
+  in intercalate "\n" [[getPixel x y | x <- [0..8]] | y <- [maxY,maxY-1..0]]
+
+getHeight :: Field -> Int
+getHeight = getMaxY
+
 getStartPos :: Field -> Pos
 getStartPos field = (2, getMaxY field + 4)
 
-canMove :: (Pos -> Pos) -> Field -> (Block, Pos) -> Bool
-canMove adjust field (block, pos) =
+canMove :: (Pos -> Pos) -> Field -> Block -> Pos -> Bool
+canMove adjust field block pos =
   let blockPos = materialize block pos
       blockPos' = S.map adjust blockPos
       isNotBlockedByFieldRock = S.disjoint field blockPos'
@@ -55,15 +86,58 @@ canMove adjust field (block, pos) =
       isNotBlocked = isNotBlockedByFieldRock && isNotBlockedByLeftWall && isNotBlockedByRightWall && isNotBlockedByFloor
   in isNotBlocked
 
-canMoveDown :: Field -> (Block, Pos) -> Bool
+canMoveDown :: Field -> Block -> Pos -> Bool
 canMoveDown = canMove (\(x, y) -> (x, y - 1))
 
-canMoveLeft :: Field -> (Block, Pos) -> Bool
+canMoveLeft :: Field -> Block -> Pos -> Bool
 canMoveLeft = canMove (\(x, y) -> (x - 1, y))
 
-canMoveRight :: Field -> (Block, Pos) -> Bool
+canMoveRight :: Field -> Block -> Pos -> Bool
 canMoveRight = canMove (\(x, y) -> (x + 1, y))
+
+applyJet :: Field -> Jet -> Block -> Pos -> Pos
+applyJet field JetLeft block pos = if canMoveLeft field block pos then getLeftPos pos else pos
+applyJet field JetRight block pos = if canMoveRight field block pos then getRightPos pos else pos
+
+takeBlockTurn :: Field -> [Jet] -> Block -> Pos -> (Field, [Jet])
+takeBlockTurn _ [] _ _ = undefined
+takeBlockTurn field (jet:jets) block pos =
+  let pos' = applyJet field jet block pos
+  in
+    if not (canMoveDown field block pos') then
+      let blocks = materialize block pos'
+          field' = S.union field blocks
+      in (field', jets)
+    else
+      let pos'' = getDownPos pos'
+      in takeBlockTurn field jets block pos''
+
+takeBlocksTurn :: Field -> [Jet] -> [Block] -> Int -> Field
+-- takeBlocksTurn field jets blocks blocksLeft | trace (show blocksLeft) False = undefined
+takeBlocksTurn field _ _ 0 = field
+takeBlocksTurn _ _ [] _ = undefined
+takeBlocksTurn field jets (block:blocks) blocksLeft =
+  let startPos = getStartPos field
+      (field', jets') = takeBlockTurn field jets block startPos
+  in takeBlocksTurn field' jets' blocks (blocksLeft-1)
 
 day17 :: IO ()
 day17 = do
-  putStrLn "day17"
+  let input = testInput
+  -- input <- readFile "input/Day17.txt"
+
+  jets <- case regularParse jetsParser input of
+    Left e -> fail $ show e
+    Right xs -> pure $ mkJets xs
+
+  let field = mkField
+      blocks = mkBlocks
+
+  -- part 1
+  let field' = takeBlocksTurn field jets blocks 2
+      height = getHeight field'
+
+  putStrLn "Field:"
+  putStrLn $ drawField field'
+
+  putStrLn $ "Height: " <> show height

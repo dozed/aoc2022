cafd79309daf23ad6f1cb3e534011b2bc5abf26d
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-01-28 21:55:00 +0100

    Working on day 19

--- src/Day19.hs
+++ src/Day19.hs
@@ -3,6 +3,7 @@
 module Day19 where
 
 import Control.Monad (void)
+import Data.Semigroup (stimes)
 import Text.Parsec hiding (count)
 import Text.Parsec.String
 import Text.ParserCombinators.Parsec.Number (int)
@@ -32,23 +33,23 @@ blueprintParser = do
   void $ string " ore and "
   obsidianCost4 <- int
   void $ string " obsidian."
-  let oreRobotCosts = RobotCosts { oreCost = oreCost1, clayCost = 0, obsidianCost = 0 }
-      clayRobotCosts = RobotCosts { oreCost = oreCost2, clayCost = 0, obsidianCost = 0 }
-      obsidianRobotCosts = RobotCosts { oreCost = oreCost3, clayCost = clayCost3, obsidianCost = 0 }
-      geodeRobotCosts = RobotCosts { oreCost = oreCost4, clayCost = 0, obsidianCost = obsidianCost4 }
+  let oreRobotCosts = RobotCost { oreCost = oreCost1, clayCost = 0, obsidianCost = 0 }
+      clayRobotCosts = RobotCost { oreCost = oreCost2, clayCost = 0, obsidianCost = 0 }
+      obsidianRobotCosts = RobotCost { oreCost = oreCost3, clayCost = clayCost3, obsidianCost = 0 }
+      geodeRobotCosts = RobotCost { oreCost = oreCost4, clayCost = 0, obsidianCost = obsidianCost4 }
       blueprint = Blueprint {
         blueprintId = blueprintId,
-        oreRobotCosts = oreRobotCosts,
-        clayRobotCosts = clayRobotCosts,
-        obsidianRobotCosts = obsidianRobotCosts,
-        geodeRobotCosts = geodeRobotCosts
+        oreRobotCost = oreRobotCosts,
+        clayRobotCost = clayRobotCosts,
+        obsidianRobotCost = obsidianRobotCosts,
+        geodeRobotCost = geodeRobotCosts
       }
   return blueprint
 
 blueprintsParser :: Parser [Blueprint]
 blueprintsParser = endBy1 blueprintParser endOfLine
 
-data Robot = Ore | Clay | Obsidian | Geode
+data RobotType = OreRobot | ClayRobot | ObsidianRobot | GeodeRobot
              deriving (Eq, Show)
 
 type Amount = Int
@@ -75,19 +76,95 @@ collectMaterials i = i {
 
 type Cost = Int
 
-data RobotCosts = RobotCosts {
+data RobotCost = RobotCost {
   oreCost :: Cost,
   clayCost :: Cost,
   obsidianCost :: Cost
 } deriving (Eq, Show)
 
+instance Semigroup RobotCost where
+  (<>) rc1 rc2 =
+      RobotCost {
+        oreCost = oreCost rc1 + oreCost rc2,
+        clayCost = clayCost rc1 + clayCost rc2,
+        obsidianCost = obsidianCost rc1 + obsidianCost rc2
+      }
+
+  stimes n rc =
+    RobotCost {
+      oreCost = oreCost rc * fromIntegral n,
+      clayCost = clayCost rc * fromIntegral n,
+      obsidianCost = obsidianCost rc * fromIntegral n
+    }
+
+instance Monoid RobotCost where
+  mempty = RobotCost { oreCost = 0, clayCost = 0, obsidianCost = 0 }
+  mappend = (<>)
+
 data Blueprint = Blueprint {
   blueprintId :: Int,
-  oreRobotCosts :: RobotCosts,
-  clayRobotCosts :: RobotCosts,
-  obsidianRobotCosts :: RobotCosts,
-  geodeRobotCosts :: RobotCosts
+  oreRobotCost :: RobotCost,
+  clayRobotCost :: RobotCost,
+  obsidianRobotCost :: RobotCost,
+  geodeRobotCost :: RobotCost
 } deriving (Eq, Show)
+
+getMaximumBuildableRobots' :: RobotCost -> Inventory -> Int
+getMaximumBuildableRobots' rc i =
+  let maxByOre = if oreCost rc == 0 then 0 else oreAmount i `div` oreCost rc
+      maxByClay = if clayCost rc == 0 then 0 else clayAmount i `div` clayCost rc
+      maxByObsidian = if obsidianCost rc == 0 then 0 else obsidianAmount i `div` obsidianCost rc
+  in minimum [maxByOre, maxByClay, maxByObsidian]
+
+getMaximumBuildableRobots :: Blueprint -> Inventory -> RobotType -> Int
+getMaximumBuildableRobots bp i OreRobot = getMaximumBuildableRobots' (oreRobotCost bp) i
+getMaximumBuildableRobots bp i ClayRobot = getMaximumBuildableRobots' (clayRobotCost bp) i
+getMaximumBuildableRobots bp i ObsidianRobot = getMaximumBuildableRobots' (obsidianRobotCost bp) i
+getMaximumBuildableRobots bp i GeodeRobot = getMaximumBuildableRobots' (geodeRobotCost bp) i
+
+data RobotBuild = RobotBuild {
+  buildOreRobots :: Number,
+  buildClayRobots :: Number,
+  buildObsidianRobots :: Number,
+  buildGeodeRobots :: Number
+} deriving (Eq, Show)
+
+addRobotsToInventory :: RobotType -> Number -> Inventory -> Inventory
+addRobotsToInventory OreRobot n i = i { numOreRobots = numOreRobots i + n }
+addRobotsToInventory ClayRobot n i = i { numClayRobots = numClayRobots i + n }
+addRobotsToInventory ObsidianRobot n i = i { numObsidianRobots = numObsidianRobots i + n }
+addRobotsToInventory GeodeRobot n i = i { numGeodeRobots = numGeodeRobots i + n }
+
+isBuildable :: Blueprint -> Inventory -> RobotBuild -> Bool
+isBuildable bp i rb =
+  let oreCosts = stimes (buildOreRobots rb) (oreRobotCost bp)
+      clayCosts = stimes (buildClayRobots rb) (clayRobotCost bp)
+      obsidianCosts = stimes (buildObsidianRobots rb) (obsidianRobotCost bp)
+      robotsCosts = mconcat [oreCosts, clayCosts, obsidianCosts]
+  in oreAmount i >= oreCost robotsCosts && clayAmount i >= clayCost robotsCosts && obsidianAmount i >= obsidianCost robotsCosts
+
+getBuildableRobots :: Blueprint -> Inventory -> [RobotBuild]
+getBuildableRobots bp i = do
+  let maxOreRobots = getMaximumBuildableRobots bp i OreRobot
+      maxClayRobots = getMaximumBuildableRobots bp i ClayRobot
+      maxObsidianRobots = getMaximumBuildableRobots bp i ObsidianRobot
+      maxGeodeRobots = getMaximumBuildableRobots bp i GeodeRobot
+
+  numOreRobots <- [0..maxOreRobots]
+  numClayRobots <- [0..maxClayRobots]
+  numObsidianRobots <- [0..maxObsidianRobots]
+  numGeodeRobots <- [0..maxGeodeRobots]
+
+  let build = RobotBuild {
+    buildOreRobots = numOreRobots,
+    buildClayRobots = numClayRobots,
+    buildObsidianRobots = numObsidianRobots,
+    buildGeodeRobots = numGeodeRobots
+  }
+
+  build' <- if isBuildable bp i build then [build] else []
+
+  return build'
 
 day19 :: IO ()
 day19 = do
--- test/Day19Spec.hs
+++ test/Day19Spec.hs
@@ -1,26 +1,44 @@
 module Day19Spec where
 
 import Test.Hspec
+import Test.Hspec.Checkers
+import Test.QuickCheck
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Checkers
 
 import Day19
 import Util (regularParse)
 
+instance Arbitrary RobotCost where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return RobotCost { oreCost = a, clayCost = b, obsidianCost = c }
+
+instance EqProp RobotCost where
+  (=-=) = eq
+
 day19Spec :: Spec
 day19Spec = do
 
+  describe "RobotCost" $ do
+    testBatch (semigroup (undefined :: RobotCost, undefined :: Int))
+    testBatch (monoid (undefined :: RobotCost))
+
   describe "blueprintsParser" $ do
     it "should parse a list of Blueprint" $ do
-      let oreRobotCosts1 = RobotCosts { oreCost = 4, clayCost = 0, obsidianCost = 0 }
-          clayRobotCosts1 = RobotCosts { oreCost = 2, clayCost = 0, obsidianCost = 0 }
-          obsidianRobotCosts1 = RobotCosts { oreCost = 3, clayCost = 14, obsidianCost = 0 }
-          geodeRobotCosts1 = RobotCosts { oreCost = 2, clayCost = 0, obsidianCost = 7 }
-          blueprint1 = Blueprint { blueprintId = 1, oreRobotCosts = oreRobotCosts1, clayRobotCosts = clayRobotCosts1, obsidianRobotCosts = obsidianRobotCosts1, geodeRobotCosts = geodeRobotCosts1 }
+      let oreRobotCosts1 = RobotCost { oreCost = 4, clayCost = 0, obsidianCost = 0 }
+          clayRobotCosts1 = RobotCost { oreCost = 2, clayCost = 0, obsidianCost = 0 }
+          obsidianRobotCosts1 = RobotCost { oreCost = 3, clayCost = 14, obsidianCost = 0 }
+          geodeRobotCosts1 = RobotCost { oreCost = 2, clayCost = 0, obsidianCost = 7 }
+          blueprint1 = Blueprint { blueprintId = 1, oreRobotCost = oreRobotCosts1, clayRobotCost = clayRobotCosts1, obsidianRobotCost = obsidianRobotCosts1, geodeRobotCost = geodeRobotCosts1 }
 
-      let oreRobotCosts2 = RobotCosts { oreCost = 2, clayCost = 0, obsidianCost = 0 }
-          clayRobotCosts2 = RobotCosts { oreCost = 3, clayCost = 0, obsidianCost = 0 }
-          obsidianRobotCosts2 = RobotCosts { oreCost = 3, clayCost = 8, obsidianCost = 0 }
-          geodeRobotCosts2 = RobotCosts { oreCost = 3, clayCost = 0, obsidianCost = 12 }
-          blueprint2 = Blueprint { blueprintId = 2, oreRobotCosts = oreRobotCosts2, clayRobotCosts = clayRobotCosts2, obsidianRobotCosts = obsidianRobotCosts2, geodeRobotCosts = geodeRobotCosts2 }
+      let oreRobotCosts2 = RobotCost { oreCost = 2, clayCost = 0, obsidianCost = 0 }
+          clayRobotCosts2 = RobotCost { oreCost = 3, clayCost = 0, obsidianCost = 0 }
+          obsidianRobotCosts2 = RobotCost { oreCost = 3, clayCost = 8, obsidianCost = 0 }
+          geodeRobotCosts2 = RobotCost { oreCost = 3, clayCost = 0, obsidianCost = 12 }
+          blueprint2 = Blueprint { blueprintId = 2, oreRobotCost = oreRobotCosts2, clayRobotCost = clayRobotCosts2, obsidianRobotCost = obsidianRobotCosts2, geodeRobotCost = geodeRobotCosts2 }
 
       let expectedBlueprints = [blueprint1, blueprint2]
 

2342405d4adbf39fb0c1617c9dbf71de0509e013
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-02-12 21:05:50 +0100

    Day 20 part 2

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE QuasiQuotes #-}
 
 module Day20 where
@@ -10,14 +6,13 @@ import Control.Monad (foldM, forM_)
 import qualified Data.Vector as V
 import Data.Vector.Mutable (IOVector)
 import qualified Data.Vector.Mutable as MV
-import Data.List (elemIndex)
 import Data.Maybe (fromJust)
 import Text.Parsec hiding (count)
 import Text.Parsec.String
 import Text.ParserCombinators.Parsec.Number (int)
 import Text.RawString.QQ
 
-import Util (lstrip, move, regularParse, swap, swap')
+import Util (lstrip, regularParse, swap')
 
 testInput :: String
 testInput = lstrip [r|
@@ -40,76 +35,20 @@ type Length = Int
 type Index = Int
 type Offset = Int
 
--- list/position-based approach
--- len: 8
--- pos: 2
---
--- 0 1 2 3 4 5 6 7
---     ^
---
--- offset: 7
--- pos': 1 = (2 + 7) mod 8
---
--- offset: -3
--- pos': 7 = (2 + (8 - (abs (-3) `mod` 8))) `mod` 8 = (2 + 5)
---
--- offset: -13
--- pos': 5 = (2 + (8 - (abs (-13) `mod` 8))) `mod` 8 = (2 + (8 - 5)) = (2 + 3)
-applyOffset :: Length -> Index -> Offset -> Index
-applyOffset len pos offset
-  | offset >= 0 = (pos + offset) `mod` len
-  | otherwise = (pos + (len - (abs offset `mod` len))) `mod` len
-
-mixOne :: [IdInt] -> IdInt -> [IdInt]
-mixOne xs el@(IdInt _ offset) =
-  let from = fromJust . elemIndex el $ xs
-      len = length xs
-      to = applyOffset len from offset
-      to' =
-        if to == 0 then len - 1
-        else if offset < 0 && to > from then to - 1
-        else if offset > 0 && to < from then to + 1
-        else to
-      xs' = move from to' xs
-  in xs'
-
-mix :: [IdInt] -> [IdInt] -> [IdInt]
-mix reference current = foldl mixOne current reference
-
-mixN :: Int -> [IdInt] -> [IdInt] -> [IdInt]
-mixN n reference current = foldl mixOne current (take n reference)
-
--- list/shift-based approach
-shift :: Offset -> Index -> [a] -> [a]
-shift _ _ [] = []
-shift 0 _ xs = xs
-shift offset from xs | offset > 0 =
+cycleResetPrint :: [IdInt] -> IO ()
+cycleResetPrint xs = do
   let len = length xs
-      to = if from == len - 1 then 0 else from + 1
-      xs' = swap from to xs
-      xs'' = shift (offset-1) to xs'
-  in xs''
-shift offset from xs =
-  let len = length xs
-      to = if from == 0 then len - 1 else from - 1
-      xs' = swap from to xs
-      xs'' = shift (offset+1) to xs'
-  in xs''
+      xs' = take len . dropWhile (\(IdInt _ x) -> x /= 0) . cycle $ xs
+  print xs'
 
-mixOne' :: [IdInt] -> IdInt -> IO [IdInt]
-mixOne' xs el@(IdInt _ offset) = do
-  let from = fromJust . elemIndex el $ xs
-      !xs' = shift offset from xs
-  print el
-  return xs'
+getSmallOffset :: Offset -> Length -> Offset
+getSmallOffset offset len
+  | offset >= 0 = (offset + (offset `div` (len-1))) `mod` len
+  | otherwise   = -((abs offset + (abs offset `div` (len-1))) `mod` len)
 
-mix' :: [IdInt] -> IO [IdInt]
-mix' idInts = foldM mixOne' idInts idInts
-
--- Vector/shift-based approach
-shift' :: Offset -> Index -> IOVector a -> IO ()
-shift' 0 _ _ = return ()
-shift' offset from vec = do
+shift :: Offset -> Index -> IOVector a -> IO ()
+shift 0 _ _ = return ()
+shift offset from vec = do
   let len = MV.length vec
       to = if offset > 0 then
              if from == len - 1 then 0 else from + 1
@@ -118,69 +57,54 @@ shift' offset from vec = do
       offset' = if offset > 0 then offset - 1
                 else offset + 1
   swap' from to vec
-  shift' offset' to vec
+  shift offset' to vec
 
-elemIndex' :: Eq a => IOVector a -> a -> Index -> IO (Maybe Int)
-elemIndex' vec _ i | i == MV.length vec = return Nothing
-elemIndex' vec a i = do
+elemIndex :: Eq a => IOVector a -> a -> Index -> IO (Maybe Int)
+elemIndex vec _ i | i == MV.length vec = return Nothing
+elemIndex vec a i = do
   a' <- MV.read vec i
   if a' == a then return (Just i)
-  else elemIndex' vec a (i+1)
+  else elemIndex vec a (i+1)
 
-mixOne'' :: IOVector IdInt -> IdInt -> IO ()
-mixOne'' vec el@(IdInt _ offset) = do
-  from <- fromJust <$> elemIndex' vec el 0
+mixOne :: IOVector IdInt -> IdInt -> IO ()
+mixOne vec el@(IdInt _ offset) = do
+  from <- fromJust <$> elemIndex vec el 0
   let len = MV.length vec
-      offset' = if offset >= 0 then (offset + (offset `div` len)) `mod` len
-                else -((abs offset + (abs offset `div` len)) `mod` len)
-  shift' offset' from vec
+      offset' = getSmallOffset offset len
+  shift offset' from vec
   print el
   return ()
 
-cycleResetPrint :: [IdInt] -> IO ()
-cycleResetPrint xs = do
-  let len = length xs
-      xs' = take len . dropWhile (\(IdInt _ x) -> x /= 0) . cycle $ xs
-  print xs'
-
-mix'' :: [IdInt] -> [IdInt] -> IO [IdInt]
-mix'' reference current = do
+mix :: [IdInt] -> [IdInt] -> IO [IdInt]
+mix reference current = do
   vec <- V.thaw $ V.fromList current
   forM_ reference $ \i -> do
-    mixOne'' vec i
+    mixOne vec i
   vec' <- V.freeze vec
   let xs = V.toList vec'
   cycleResetPrint xs
   return xs
 
-mixN'' :: Int -> [IdInt] -> [IdInt] -> IO [IdInt]
-mixN'' n reference current = do
-  vec <- V.thaw $ V.fromList current
-  forM_ (take n reference) $ \i ->
-    mixOne'' vec i
-  vec' <- V.freeze vec
-  return $ V.toList vec'
-
 day20 :: IO ()
 day20 = do
-  let input = testInput
-  -- input <- readFile "input/Day20.txt"
+  -- let input = testInput
+  input <- readFile "input/Day20.txt"
 
   ints <- case regularParse parseInts input of
     Left e -> fail $ show e
     Right xs -> pure xs
 
-  let idInts = zipWith IdInt [0..] ints
+  let reference = zipWith IdInt [0..] ints
 
   -- part 1
   putStrLn "--- part 1 ---"
-  idInts' <- mix'' idInts idInts
+  mixed <- mix reference reference
 
-  let idInts'' = dropWhile (\(IdInt _ x) -> x /= 0) . cycle $ idInts'
+  let mixed' = dropWhile (\(IdInt _ x) -> x /= 0) . cycle $ mixed
 
-  let (IdInt _ i) = idInts'' !! 1000
-      (IdInt _ j) = idInts'' !! 2000
-      (IdInt _ k) = idInts'' !! 3000
+  let (IdInt _ i) = mixed' !! 1000
+      (IdInt _ j) = mixed' !! 2000
+      (IdInt _ k) = mixed' !! 3000
 
   putStrLn $ "i: " <> show i
   putStrLn $ "j: " <> show j
@@ -193,9 +117,9 @@ day20 = do
   -- part 2
   putStrLn "--- part 2 ---"
   let decryptionKey = 811589153
-      reference = map (\(IdInt i x) -> IdInt i (x * decryptionKey)) idInts
+      reference' = map (\(IdInt i x) -> IdInt i (x * decryptionKey)) reference
 
-  idInts''' <- foldM (\current _ -> mix'' reference current) reference [1..10]
+  idInts''' <- foldM (\current _ -> mix reference' current) reference' [1..10]
 
   let idInts'''' = dropWhile (\(IdInt _ x) -> x /= 0) . cycle $ idInts'''
 
--- test/Day20Spec.hs
+++ test/Day20Spec.hs
@@ -2,6 +2,7 @@
 
 module Day20Spec (day20Spec) where
 
+import Control.Monad (forM_)
 import qualified Data.Vector as V
 
 import Test.Hspec
@@ -24,7 +25,7 @@ compareNumbers actual expected = do
 mixOneTest :: [IdInt] -> IdInt -> IO [IdInt]
 mixOneTest idInts idInt = do
   vec <- V.thaw $ V.fromList idInts
-  mixOne'' vec idInt
+  mixOne vec idInt
   vec' <- V.freeze vec
   return $ V.toList vec'
 
@@ -39,142 +40,80 @@ day20Spec = do
 
       numbers `shouldBe` [1, 2, -3, 3, -2, 0, 4]
 
-  describe "applyOffset" $ do
-    it "should compute a new index by applying an offset to an index in a cyclic list" $ do
-      applyOffset 5 2 13 `shouldBe` 0
-      applyOffset 5 2 3 `shouldBe` 0
+  describe "getSmallOffset" $ do
+    it "should compute the small equivalent offset" $ do
+      getSmallOffset 3 5 `shouldBe` 3
+      getSmallOffset 7 5 `shouldBe` 3
+      getSmallOffset 11 5 `shouldBe` 3
+      getSmallOffset 15 5 `shouldBe` 3
+      getSmallOffset 19 5 `shouldBe` 3
+      getSmallOffset 23 5 `shouldBe` 3
+      getSmallOffset 27 5 `shouldBe` 3
+      getSmallOffset 31 5 `shouldBe` 3
 
-      applyOffset 7 0 1 `shouldBe` 1
-      applyOffset 7 0 2 `shouldBe` 2
-      applyOffset 7 4 (-2) `shouldBe` 2
-      applyOffset 7 1 (-3) `shouldBe` 5
-
-      applyOffset 8 2 7 `shouldBe` 1
-      applyOffset 8 2 (-3) `shouldBe` 7
-      applyOffset 8 2 (-13) `shouldBe` 5
-      applyOffset 6 3 (-2) `shouldBe` 1
-      applyOffset 7 6 4 `shouldBe` 3
-      applyOffset 7 3 2 `shouldBe` 5
-      applyOffset 7 3 (-2) `shouldBe` 1
-
-  describe "mixOne" $ do
-    it "should move an element by a specific offset" $ do
-      let xs = [IdInt 0 2, IdInt 1 7, IdInt 2 1, IdInt 3 3, IdInt 4 5]
-          expected = [IdInt 0 2, IdInt 2 1, IdInt 3 3, IdInt 1 7, IdInt 4 5]
-
-      let actual = mixOne xs (IdInt 1 7)
-
-      actual `shouldBe` expected
-
-    it "should move elements by a specific offset" $ do
-      let xs1 = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 3 3, IdInt 4 (-2), IdInt 5 0, IdInt 6 4]
-          xs2 = mixOne xs1 (IdInt 0 1)
-          xs3 = mixOne xs2 (IdInt 1 2)
-          xs4 = mixOne xs3 (IdInt 2 (-3))
-          xs5 = mixOne xs4 (IdInt 3 3)
-          xs6 = mixOne xs5 (IdInt 4 (-2))
-          xs7 = mixOne xs6 (IdInt 5 0)
-          xs8 = mixOne xs7 (IdInt 6 4)
-
-      xs2 `shouldBe` [IdInt 1 2, IdInt 0 1, IdInt 2 (-3), IdInt 3 3, IdInt 4 (-2), IdInt 5 0, IdInt 6 4]
-      xs3 `shouldBe` [IdInt 0 1, IdInt 2 (-3), IdInt 1 2, IdInt 3 3, IdInt 4 (-2), IdInt 5 0, IdInt 6 4]
-      xs4 `shouldBe` [IdInt 0 1, IdInt 1 2, IdInt 3 3, IdInt 4 (-2), IdInt 2 (-3), IdInt 5 0, IdInt 6 4]
-      xs5 `shouldBe` [IdInt 0 1, IdInt 1 2, IdInt 4 (-2), IdInt 2 (-3), IdInt 5 0, IdInt 3 3, IdInt 6 4]
-      xs6 `shouldBe` [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 5 0, IdInt 3 3, IdInt 6 4, IdInt 4 (-2)]
-      xs7 `shouldBe` [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 5 0, IdInt 3 3, IdInt 6 4, IdInt 4 (-2)]
-      xs8 `shouldBe` [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 6 4, IdInt 5 0, IdInt 3 3, IdInt 4 (-2)]
-
-  describe "mix" $ do
-    it "should mix a list of IdInt" $ do
-      let idInts = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 3 3, IdInt 4 (-2), IdInt 5 0, IdInt 6 4]
-          expected = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 6 4, IdInt 5 0, IdInt 3 3, IdInt 4 (-2)]
-
-      mix idInts idInts `shouldBe` expected
+    it "should compute the small equivalent offset (2)" $ do
+      forM_ [3, 7, 11, 15, 19, 23, 27, 31] $ \offset ->
+        getSmallOffset offset 5 `shouldBe` 3
 
   describe "shift" $ do
     prop "should return same list if offset is zero" $ \(xs :: [Int], i) -> do
-      shift 0 i xs `shouldBe` xs
+      vec <- V.thaw . V.fromList $ xs
+      shift 0 i vec
+      xs' <- V.toList <$> V.freeze vec
 
-    it "should shift an element forward by a specified amount" $ do
-      let xs = [2, 1, 3, 4, -1]
+      xs' `shouldBe` xs
 
-      shift 1 0 xs `shouldBe` [1, 2, 3, 4, -1]
-      shift 2 0 xs `shouldBe` [1, 3, 2, 4, -1]
-      shift 1 4 xs `shouldBe` [-1, 1, 3, 4, 2]
-      shift 3 3 xs `shouldBe` [1, 4, 3, -1, 2]
-      shift 13 3 xs `shouldBe` [-1, 4, 2, 1, 3]
-
-    it "should shift an element backward by a specified amount" $ do
-      let xs = [2, 1, 3, 4, -1]
-
-      shift (-1) 4 xs `shouldBe` [2, 1, 3, -1, 4]
-      shift (-2) 4 xs `shouldBe` [2, 1, -1, 3, 4]
-      shift (-1) 0 xs `shouldBe` [-1, 1, 3, 4, 2]
-      shift (-2) 0 xs `shouldBe` [-1, 1, 3, 2, 4]
-      shift (-3) 1 xs `shouldBe` [-1, 2, 3, 1, 4]
-      shift (-13) 1 xs `shouldBe` [3, 4, -1, 1, 2]
-
-  describe "mix'" $ do
-    it "should mix a list of IdInt" $ do
-      let idInts = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 3 3, IdInt 4 (-2), IdInt 5 0, IdInt 6 4]
-          expected = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 6 4, IdInt 5 0, IdInt 3 3, IdInt 4 (-2)]
-
-      idInts' <- mix' idInts
-
-      compareNumbers idInts' expected
-
-  describe "mix''" $ do
-    it "should mix a list of IdInt" $ do
-      let idInts = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 3 3, IdInt 4 (-2), IdInt 5 0, IdInt 6 4]
-          expected = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 6 4, IdInt 5 0, IdInt 3 3, IdInt 4 (-2)]
-
-      idInts' <- mix'' idInts idInts
-
-      compareNumbers idInts' expected
-
-  describe "mixOne func" $ do
+  describe "mixOne" $ do
     it "should mix one forward (1)" $ do
       let idInts = [IdInt 0 0, IdInt 1 4, IdInt 2 1, IdInt 3 3, IdInt 4 5]
           idInt = IdInt 1 4
           expected = [IdInt 1 4, IdInt 2 1, IdInt 3 3, IdInt 4 5, IdInt 0 0]
 
-      idInts' <- mixOneTest idInts idInt
+      actual <- mixOneTest idInts idInt
 
-      compareNumbers idInts' expected
+      compareNumbers actual expected
 
     it "should mix one forward (2)" $ do
       let idInts = [IdInt 0 0, IdInt 1 3, IdInt 2 1, IdInt 3 3, IdInt 4 5]
           expected = [IdInt 0 0, IdInt 2 1, IdInt 3 3, IdInt 4 5, IdInt 1 3]
           idInt = idInts !! 1
 
-      idInts' <- mixOneTest idInts idInt
+      actual <- mixOneTest idInts idInt
 
-      compareNumbers idInts' expected
+      compareNumbers actual expected
 
     it "should mix one forward (3)" $ do
       let idInts = [IdInt 0 0, IdInt 1 15, IdInt 2 1, IdInt 3 3, IdInt 4 5]
           expected = [IdInt 0 0, IdInt 2 1, IdInt 3 3, IdInt 4 5, IdInt 1 15]
           idInt = idInts !! 1
 
-      idInts' <- mixOneTest idInts idInt
+      actual <- mixOneTest idInts idInt
 
-      compareNumbers idInts' expected
+      compareNumbers actual expected
 
     it "should mix one backward (1)" $ do
       let idInts = [IdInt 0 0, IdInt 1 4, IdInt 2 1, IdInt 3 (-3), IdInt 4 5]
           idInt = IdInt 3 (-3)
           expected = [IdInt 3 (-3), IdInt 0 0, IdInt 1 4, IdInt 2 1, IdInt 4 5]
 
-      idInts' <- mixOneTest idInts idInt
+      actual <- mixOneTest idInts idInt
 
-      compareNumbers idInts' expected
+      compareNumbers actual expected
 
     it "should mix one backward (2)" $ do
       let idInts = [IdInt 0 0, IdInt 1 4, IdInt 2 1, IdInt 3 (-15), IdInt 4 5]
           idInt = IdInt 3 (-15)
           expected = [IdInt 3 (-15), IdInt 0 0, IdInt 1 4, IdInt 2 1, IdInt 4 5]
 
-      idInts' <- mixOneTest idInts idInt
+      actual <- mixOneTest idInts idInt
 
-      compareNumbers idInts' expected
+      compareNumbers actual expected
 
+  describe "mix" $ do
+    it "should mix a list of IdInt" $ do
+      let idInts = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 3 3, IdInt 4 (-2), IdInt 5 0, IdInt 6 4]
+          expected = [IdInt 0 1, IdInt 1 2, IdInt 2 (-3), IdInt 6 4, IdInt 5 0, IdInt 3 3, IdInt 4 (-2)]
+
+      actual <- mix idInts idInts
+
+      compareNumbers actual expected

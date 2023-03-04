43a4630e8e76617bf484906590085900976710e3
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-03-04 17:44:06 +0100

    Working on day 24 part 1

--- src/Day24.hs
+++ src/Day24.hs
@@ -10,6 +10,10 @@
 
 module Day24 where
 
+import Control.Monad (forM_, when)
+import Data.Heap (Heap)
+import qualified Data.Heap as H
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
 import Data.List (sort)
 import Data.Map (Map)
 import qualified Data.Map as M
@@ -19,7 +23,7 @@ import qualified Data.Set as S
 import Optics.Core (set)
 import Optics.TH
 
-import Util (replaceAtIndex)
+import Util (filterNot, replaceAtIndex)
 
 testInput :: [String]
 testInput = [
@@ -32,12 +36,24 @@ testInput = [
     "#####.#"
   ]
 
+testInput2 :: [String]
+testInput2 = [
+    "#.######",
+    "#>>.<^<#",
+    "#.<..<<#",
+    "#>v.><>#",
+    "#<^v^^>#",
+    "######.#"
+  ]
+
 type X = Int
 type Y = Int
 type Pos = (X, Y)
 
 data Direction = N | S | E | W
   deriving (Eq, Ord, Show)
+
+data Move = Move Direction | Wait
 
 getAdjacentPos :: Pos -> Direction -> Pos
 getAdjacentPos (x, y) N = (x, y - 1)
@@ -143,13 +159,57 @@ moveBlizzards field =
       field' = foldl (\acc i -> moveBlizzard' acc i) field [0..(numBlizzards-1)]
   in field'
 
+possibleMoves :: [Move]
+possibleMoves = [Move N, Move S, Move E, Move W, Wait]
+
+applyMove :: Move -> Pos -> Pos
+applyMove (Move d) pos = getAdjacentPos pos d 
+applyMove Wait pos = pos 
+
+type Minute = Int
+
+isOutside :: Pos -> Bool
+isOutside (x, y) = y < 1
+
+data SearchNode = SearchNode Field [Pos] deriving Eq
+
+getPath :: SearchNode -> [Pos]
+getPath (SearchNode _ path) = path
+
+instance Ord SearchNode where
+  a <= b = length (getPath a) <= length (getPath b)
+
+go :: Field -> Pos -> Set Pos -> IORef Minute -> Minute -> IO ()
+go field pos _ minMinuteRef minute | field.endPos == pos =  do
+  minMinute <- readIORef minMinuteRef
+  when (minute < minMinute) $ do
+    writeIORef minMinuteRef minute
+    print minute
+go field pos visited minMinuteRef minute = do
+  print pos
+  let
+    field' = moveBlizzards field
+    moves = possibleMoves
+    nextPositions1 = map (flip applyMove pos) moves
+    nextPositions2 = filterNot (\p -> isOutside p) nextPositions1
+    -- nextPositions3 = filterNot (\p -> S.member p visited) nextPositions2
+    nextPositions4 = filterNot (\p -> isWallAt field' p) nextPositions2
+    nextPositions5 = filterNot (\p -> isBlizzardAt field' p) nextPositions4
+  if null nextPositions5 then return () -- putStrLn "empty"
+  else forM_ nextPositions5 $ \p -> go field' p (S.insert p visited) minMinuteRef (minute+1)
+
 day24 :: IO ()
 day24 = do
-  let input = testInput
+  -- let input = testInput
+  let input = testInput2
 
   putStrLn "day24"
 
   let field = readField input
   print field
+
+  minMinuteRef <- newIORef 1000000
+
+  go field field.startPos (S.singleton field.startPos) minMinuteRef 1
 
   return ()
